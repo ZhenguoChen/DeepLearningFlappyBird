32be405a012d8163fa056be48f361e0cdc2cd3c2
ZhenguoChen <zhenguo.chen@colorado.edu>
2024-03-30 20:18:22 -0400

    formatting

--- deep_q_network.py
+++ deep_q_network.py
@@ -1,42 +1,60 @@
 #!/usr/bin/env python
-from __future__ import print_function
-
-import tensorflow as tf
-import cv2
+import os
 import sys
+
+import cv2
+import tensorflow as tf
+
 sys.path.append("game/")
-import wrapped_flappy_bird as game
 import random
-import numpy as np
 from collections import deque
 
-GAME = 'bird' # the name of the game being played for log files
-ACTIONS = 2 # number of valid actions
-GAMMA = 0.99 # decay rate of past observations
-OBSERVE = 100000. # timesteps to observe before training
-EXPLORE = 2000000. # frames over which to anneal epsilon
-FINAL_EPSILON = 0.0001 # final value of epsilon
-INITIAL_EPSILON = 0.0001 # starting value of epsilon
-REPLAY_MEMORY = 50000 # number of previous transitions to remember
+import numpy as np
+import wrapped_flappy_bird as game
+
+MODE = os.environ.get("MODE", "EVAL")
+
+GAME = "bird"  # the name of the game being played for log files
+ACTIONS = 2  # number of valid actions
+GAMMA = 0.99  # decay rate of past observations
+REPLAY_MEMORY = 50000  # number of previous transitions to remember
 MAX_EPOCHS = 1000000
-BATCH = 32 # size of minibatch
+BATCH = 32  # size of minibatch
 FRAME_PER_ACTION = 1
+
+if MODE == "TRAIN":
+    OBSERVE = 10000
+    EXPLORE = 3000000
+    FINAL_EPSILON = 0.0001
+    INITIAL_EPSILON = 0.1
+else:
+    OBSERVE = 100000. # timesteps to observe before training
+    EXPLORE = 2000000. # frames over which to anneal epsilon
+    FINAL_EPSILON = 0.0001 # final value of epsilon
+    INITIAL_EPSILON = 0.0001 # starting value of epsilon
 
 tf.compat.v1.disable_eager_execution()
 
+
 def weight_variable(shape):
-    initial = tf.random.truncated_normal(shape, stddev = 0.01)
+    initial = tf.random.truncated_normal(shape, stddev=0.01)
     return tf.Variable(initial)
+
 
 def bias_variable(shape):
-    initial = tf.constant(0.01, shape = shape)
+    initial = tf.constant(0.01, shape=shape)
     return tf.Variable(initial)
 
+
 def conv2d(x, W, stride):
-    return tf.nn.conv2d(x, filters=W, strides = [1, stride, stride, 1], padding = "SAME")
+    return tf.nn.conv2d(x, filters=W, strides=[1, stride, stride, 1], padding="SAME")
+
 
 def max_pool_2x2(x):
-    return tf.nn.max_pool2d(input=x, ksize = [1, 2, 2, 1], strides = [1, 2, 2, 1], padding = "SAME")
+    return tf.nn.max_pool2d(
+        input=x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding="SAME"
+    )
+
 
 def createNetwork():
     # network weights
@@ -63,12 +81,12 @@ def createNetwork():
     h_pool1 = max_pool_2x2(h_conv1)
 
     h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2, 2) + b_conv2)
-    #h_pool2 = max_pool_2x2(h_conv2)
+    # h_pool2 = max_pool_2x2(h_conv2)
 
     h_conv3 = tf.nn.relu(conv2d(h_conv2, W_conv3, 1) + b_conv3)
-    #h_pool3 = max_pool_2x2(h_conv3)
+    # h_pool3 = max_pool_2x2(h_conv3)
 
-    #h_pool3_flat = tf.reshape(h_pool3, [-1, 256])
+    # h_pool3_flat = tf.reshape(h_pool3, [-1, 256])
     h_conv3_flat = tf.reshape(h_conv3, [-1, 1600])
 
     h_fc1 = tf.nn.relu(tf.matmul(h_conv3_flat, W_fc1) + b_fc1)
@@ -76,9 +94,10 @@ def createNetwork():
     # readout layer
     readout = tf.matmul(h_fc1, W_fc2) + b_fc2
 
-    return s, readout, h_fc1
+    return s, readout
 
-def trainNetwork(s, readout, h_fc1, sess):
+
+def trainNetwork(s, readout, sess):
     # define the cost function
     a = tf.compat.v1.placeholder("float", [None, ACTIONS])
     y = tf.compat.v1.placeholder("float", [None])
@@ -92,16 +111,12 @@ def trainNetwork(s, readout, h_fc1, sess):
     # store the previous observations in replay memory
     D = deque()
 
-    # printing
-    a_file = open("logs_" + GAME + "/readout.txt", 'w')
-    h_file = open("logs_" + GAME + "/hidden.txt", 'w')
-
     # get the first state by doing nothing and preprocess the image to 80x80x4
     do_nothing = np.zeros(ACTIONS)
     do_nothing[0] = 1
     x_t, r_0, terminal = game_state.frame_step(do_nothing)
     x_t = cv2.cvtColor(cv2.resize(x_t, (80, 80)), cv2.COLOR_BGR2GRAY)
-    ret, x_t = cv2.threshold(x_t,1,255,cv2.THRESH_BINARY)
+    ret, x_t = cv2.threshold(x_t, 1, 255, cv2.THRESH_BINARY)
     s_t = np.stack((x_t, x_t, x_t, x_t), axis=2)
 
     # saving and loading networks
@@ -119,7 +134,7 @@ def trainNetwork(s, readout, h_fc1, sess):
     t = 0
     while "flappy bird" != "angry bird":
         # choose an action epsilon greedily
-        readout_t = readout.eval(feed_dict={s : [s_t]})[0]
+        readout_t = readout.eval(feed_dict={s: [s_t]})[0]
         a_t = np.zeros([ACTIONS])
         action_index = 0
         if t % FRAME_PER_ACTION == 0:
@@ -131,7 +146,7 @@ def trainNetwork(s, readout, h_fc1, sess):
                 action_index = np.argmax(readout_t)
                 a_t[action_index] = 1
         else:
-            a_t[0] = 1 # do nothing
+            a_t[0] = 1  # do nothing
 
         # scale down epsilon
         if epsilon > FINAL_EPSILON and t > OBSERVE:
@@ -142,7 +157,7 @@ def trainNetwork(s, readout, h_fc1, sess):
         x_t1 = cv2.cvtColor(cv2.resize(x_t1_colored, (80, 80)), cv2.COLOR_BGR2GRAY)
         ret, x_t1 = cv2.threshold(x_t1, 1, 255, cv2.THRESH_BINARY)
         x_t1 = np.reshape(x_t1, (80, 80, 1))
-        #s_t1 = np.append(x_t1, s_t[:,:,1:], axis = 2)
+        # s_t1 = np.append(x_t1, s_t[:,:,1:], axis = 2)
         s_t1 = np.append(x_t1, s_t[:, :, :3], axis=2)
 
         # store the transition in D
@@ -162,7 +177,7 @@ def trainNetwork(s, readout, h_fc1, sess):
             s_j1_batch = [d[3] for d in minibatch]
 
             y_batch = []
-            readout_j1_batch = readout.eval(feed_dict = {s : s_j1_batch})
+            readout_j1_batch = readout.eval(feed_dict={s: s_j1_batch})
             for i in range(0, len(minibatch)):
                 terminal = minibatch[i][4]
                 # if terminal, only equals reward
@@ -172,11 +187,7 @@ def trainNetwork(s, readout, h_fc1, sess):
                     y_batch.append(r_batch[i] + GAMMA * np.max(readout_j1_batch[i]))
 
             # perform gradient step
-            train_step.run(feed_dict = {
-                y : y_batch,
-                a : a_batch,
-                s : s_j_batch}
-            )
+            train_step.run(feed_dict={y: y_batch, a: a_batch, s: s_j_batch})
 
         # update the old values
         s_t = s_t1
@@ -184,7 +195,7 @@ def trainNetwork(s, readout, h_fc1, sess):
 
         # save progress every 10000 iterations
         if t % 10000 == 0:
-            saver.save(sess, 'saved_networks/' + GAME + '-dqn', global_step = t)
+            saver.save(sess, "saved_networks/" + GAME + "-dqn", global_step=t)
 
         # print info
         state = ""
@@ -195,26 +206,41 @@ def trainNetwork(s, readout, h_fc1, sess):
         else:
             state = "train"
 
-        print("TIMESTEP", t, "/ STATE", state, \
-            "/ EPSILON", epsilon, "/ ACTION", action_index, "/ REWARD", r_t, \
-            "/ Q_MAX %e" % np.max(readout_t))
+        print(
+            "TIMESTEP",
+            t,
+            "/ STATE",
+            state,
+            "/ EPSILON",
+            epsilon,
+            "/ ACTION",
+            action_index,
+            "/ REWARD",
+            r_t,
+            "/ Q_MAX %e" % np.max(readout_t),
+        )
         # write info to files
-        '''
+        """
         if t % 10000 <= 100:
             a_file.write(",".join([str(x) for x in readout_t]) + '\n')
             h_file.write(",".join([str(x) for x in h_fc1.eval(feed_dict={s:[s_t]})[0]]) + '\n')
             cv2.imwrite("logs_tetris/frame" + str(t) + ".png", x_t1)
-        '''
+        """
         if t > MAX_EPOCHS:
             break
 
+
 def playGame():
-    sess = tf.compat.v1.InteractiveSession(config=tf.compat.v1.ConfigProto(log_device_placement=True))
-    s, readout, h_fc1 = createNetwork()
-    trainNetwork(s, readout, h_fc1, sess)
+    sess = tf.compat.v1.InteractiveSession(
+        config=tf.compat.v1.ConfigProto(log_device_placement=True)
+    )
+    s, readout = createNetwork()
+    trainNetwork(s, readout, sess)
+
 
 def main():
     playGame()
+
 
 if __name__ == "__main__":
     main()
